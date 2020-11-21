2e962e31650741674c8efb24a1fc2e8574a6d3c5
kaushal <kaushalsivasai.09@gmail.com>
2020-11-21 14:44:28 +0530

    hlo

--- sketch.js
+++ sketch.js
@@ -1,29 +1,43 @@
-var ball;
+var magicalball;
+var database,position;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    magicalball = createSprite(250,250,10,10);
+    magicalball.shapeColor = "red";
+    var magicalballPosition = database.ref("ball/position");
+    magicalballPosition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("ball/position").set({
+      "x":position.x+x,
+      "y":position.y+y
+    })
+}
+function readPosition(data){
+    position = data.val();
+    magicalball.x = position.x;
+    magicalball.y = position.y;
+}
+function showError(){
+    console.log("giveError")
 }
